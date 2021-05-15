1dd5d533d3b8cd2dca54fe22a5761e8defba80e2
anja <anja@prelle.org>
2021-05-15 15:23:51 +0200

    SR6FVTT-28: Calculate and show edge gain

--- module/dice/dice.js
+++ module/dice/dice.js
@@ -14,7 +14,7 @@ export async function doRoll(data, messageData = {}) {
       data.useWildDie = form.wilddie.checked;
       data.type = type;
       messageData.rollMode = form.rollMode.value;
-      data.weapon = false; 
+      data.weapon = false;
       if (data.modifier > 0) {
         data.formula = data.skill.pool + " + " + data.modifier + "d6";
       } else {
@@ -102,7 +102,7 @@ export async function doAttackRoll(data, messageData = {}) {
       data.useWildDie = form.wilddie.checked;
       data.type = type;
       messageData.rollMode = form.rollMode.value;
-      data.weapon = data.item ? true : false; 
+      data.weapon = data.item ? true : false;
       if (data.modifier > 0) {
         data.formula = data.skill.pool + " + " + data.modifier + "d6";
       } else {
@@ -145,9 +145,10 @@ async function _attackRollDialog({ data, foo } = {}) {
     data.modifier = 0;
   }
 
-  data.defRating= 0;
+  data.defRating = 0;
   if (data.targetId) {
-    data.extraText = game.i18n.localize("shadowrun6.roll.attack") + " " + game.actors.get(data.targetId).name + " " + game.i18n.localize("shadowrun6.roll.with") + " " + data.item.name;
+    data.targetName = game.actors.get(data.targetId).name;
+    data.extraText = game.i18n.localize("shadowrun6.roll.attack") + " " + data.targetName + " " + game.i18n.localize("shadowrun6.roll.with") + " " + data.item.name;
     data.defRating = game.actors.get(data.targetId).data.data.derived.defense_rating.pool;
   }
   // Render modal dialog
@@ -163,27 +164,58 @@ async function _attackRollDialog({ data, foo } = {}) {
     new CombatDialog({
       title: title,
       content: html,
+      target: data.targetId ? true : false,
+      targetName : data.targetName,
       buttons: {
         normal: {
           label: game.i18n.localize("shadowrun6.rollType.normal"),
           callback: html => resolve(foo(0, html[0].querySelector("form"), data))
-        },
-        bought: {
-          label: game.i18n.localize("shadowrun6.rollType.bought"),
-          callback: html => resolve(foo(1, html[0].querySelector("form"), data))
         }
       },
       default: "normal",
       render: html => console.log("Register interactivity in the rendered dialog"),
       close: () => resolve(null)
-    }).render(true);
+    }).render(true, { width: 600, height: 330 });
   });
 }
 
-export class CombatDialog extends Dialog{
+export class CombatDialog extends Dialog {
+  
   activateListeners(html) {
-    console.log("foo");
-	super.activateListeners(html);
+    super.activateListeners(html);
+    html.find('.calc-edge').show(this._onCalcEdge.bind(this));
+    if (!this.data.target) {
+      html.find('.calc-edge').show(this._onNoTarget.bind(this));
+    } 
+      html.find('.calc-edge-edit').change(this._onCalcEdge.bind(this));
+      html.find('.calc-edge-edit').keyup(this._onCalcEdge.bind(this));
+  }
+
+  _onCalcEdge(event) {
+    const arElement = document.getElementById("ar");
+    const drElement = document.getElementById("dr");
+    const dr = drElement.value;
+    const ar = arElement.children[arElement.selectedIndex].dataset.itemAr;
+    let result = ar - dr;
+    let edgeOffsetPlayer = 0;
+    let edgeOffsetTarget = 0;
+    if (result > 0) {
+      edgeOffsetPlayer = Math.min(2, Math.floor(result / 4));
+    } else {
+      edgeOffsetTarget = Math.min(2, Math.floor((result*-1) / 4));
+    }
+    if (edgeOffsetPlayer > 0) {
+      document.getElementById("edgeLabel").innerText = game.i18n.localize("shadowrun6.roll.edgegain") + edgeOffsetPlayer + " " + game.i18n.localize("shadowrun6.roll.edgegain_player");
+    } else if (edgeOffsetTarget > 0) {
+      let targetName = this.targetName ? this.targetName : game.i18n.localize("shadowrun6.roll.target");
+      document.getElementById("edgeLabel").innerText = game.i18n.localize("shadowrun6.roll.edgegain") + edgeOffsetTarget + " (" + targetName + ")" ;
+    } else {
+    document.getElementById("edgeLabel").innerText = game.i18n.localize("shadowrun6.roll.noedgegain");
+    }
+  }
+
+  _onNoTarget() {
+    document.getElementById("noTargetLabel").innerText = game.i18n.localize("shadowrun6.roll.notarget");
   }
 }
 
