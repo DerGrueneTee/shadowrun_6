4bd07b22e86bef32167aab0d484e41ae5543a51f
anja <anja@prelle.org>
2021-05-22 12:33:14 +0200

    Firemode checkboxes

--- module/sheets/SR6ItemSheet.js
+++ module/sheets/SR6ItemSheet.js
@@ -1,47 +1,55 @@
 export class SR6ItemSheet extends ItemSheet {
 
-    /** @override */
-    static get defaultOptions() {
-      return mergeObject(super.defaultOptions, {
-        classes: ["shadowrun6", "sheet", "item", "gear"],
-        template: "systems/shadowrun6-eden/templates/shadrowun6-item-sheet.html",
-        width: 500,
-        height: 600,
-      });
-    }
-  
-    /** @overrride */
-	getData() {
-		let data = super.getData();
-		data.config = CONFIG.SR6;
-		return data;
-	}
+  /** @override */
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      classes: ["shadowrun6", "sheet", "item", "gear"],
+      template: "systems/shadowrun6-eden/templates/shadrowun6-item-sheet.html",
+      width: 500,
+      height: 600,
+    });
+  }
 
-    	/**
-	 * Activate event listeners using the prepared sheet HTML
-	 * @param html {HTML}   The prepared HTML object ready to be rendered into the DOM
-	 */
-	activateListeners(html) {
+  /** @overrride */
+  getData() {
+    let data = super.getData();
+    data.config = CONFIG.SR6;
+    return data;
+  }
+
+  /**
+* Activate event listeners using the prepared sheet HTML
+* @param html {HTML}   The prepared HTML object ready to be rendered into the DOM
+*/
+  activateListeners(html) {
     // Owner Only Listeners
     if ((this.actor && this.actor.isOwner)) {
       html.find('[data-field]').change(event => {
         const element = event.currentTarget;
-        let value = element.value;
+        let value;
+        if (element.type == "checkbox") {
+          value = element.checked;
+        } else {
+          value = element.value;
+        }
         const itemId = this.object.data.id;
         const field = element.dataset.field;
         this.actor.items.get(itemId).update({ [field]: value });
       });
-
     } else if (this.isEditable) {
       html.find('[data-field]').change(event => {
         const element = event.currentTarget;
-        let value = element.value;
+        if (element.type == "checkbox") {
+          value = element.checked;
+        } else {
+          value = element.value;
+        }
         const field = element.dataset.field;
         const arrayId = element.dataset.arrayid;
         if (arrayId) {
-          this.object.update({ [field]: [,,3,,] });
+          this.object.update({ [field]: [, , 3, ,] });
         } else {
-        this.object.update({ [field]: value });
+          this.object.update({ [field]: value });
         }
       });
       html.find('[data-array-field]').change(event => {
@@ -52,21 +60,21 @@ export class SR6ItemSheet extends ItemSheet {
         let newValue = [];
         if (!(idx >= 0 && array !== "")) return;
         if (field) {
-            newValue = duplicate(array.split('.').reduce(function (prev, curr) {
-                return prev ? prev[curr] : null
-            }, this.object.data));
-            newValue[idx][field] = element.value;
+          newValue = duplicate(array.split('.').reduce(function (prev, curr) {
+            return prev ? prev[curr] : null
+          }, this.object.data));
+          newValue[idx][field] = element.value;
         } else {
-            newValue = duplicate(array.split('.').reduce(function (prev, curr) {
-                return prev ? prev[curr] : null
-            }, this.object.data));
-            newValue[idx] = element.value;
+          newValue = duplicate(array.split('.').reduce(function (prev, curr) {
+            return prev ? prev[curr] : null
+          }, this.object.data));
+          newValue[idx] = element.value;
         }
         this.object.update({ [array]: newValue });
-    });
+      });
     }
 
   }
 
-  	
+
 }
