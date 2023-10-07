8ded09933ac10e28b23d13ef83a9b7ed1018dcc0
Stefan Prelle <stefan@rpgframework.de>
2023-10-07 22:56:12 +0200

    Fix #97

--- src/Shadowrun6Actor.ts
+++ src/Shadowrun6Actor.ts
@@ -1161,7 +1161,7 @@ export class Shadowrun6Actor extends Actor {
 	_getSkillPool(skillId, spec, attrib: string | undefined = undefined) {
 		const system : SR6Actor = getSystemData(this);
 		if (!skillId) throw "Skill ID may not be undefined";
-		const skl = (system as Lifeform).skills[skillId];
+		const skl : Skill = (system as Lifeform).skills[skillId];
 		if (!skillId) {
 			throw "Unknown skill '" + skillId + "'";
 		}
@@ -1172,8 +1172,8 @@ export class Shadowrun6Actor extends Actor {
 		}
 
 		// Calculate pool
-		let value = skl.points + skl.modifier;
-		if (skl.base == 0) {
+		let value : number = skl.points + skl.modifier;
+		if (skl.points == 0) {
 			if (skillDef.useUntrained) {
 				value -= 1;
 			} else return 0;
@@ -1188,7 +1188,7 @@ export class Shadowrun6Actor extends Actor {
 		}
 
 		// Add attribute
-		value = parseInt(value);
+		value = parseInt(""+value);
 		value += parseInt((system as Lifeform).attributes[attrib].pool);
 
 		return value;
